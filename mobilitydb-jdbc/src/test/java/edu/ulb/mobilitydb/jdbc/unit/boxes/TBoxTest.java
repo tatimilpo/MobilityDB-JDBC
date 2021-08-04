93a71a2a3f90d15081657d8facc5583c32861e04
Tatiana Millan <tatimilpo@gmail.com>
2021-08-05 01:51:45 +0200

    Remove unused refernce and fixing typo in TBox messages

--- mobilitydb-jdbc/src/test/java/edu/ulb/mobilitydb/jdbc/unit/boxes/TBoxTest.java
+++ mobilitydb-jdbc/src/test/java/edu/ulb/mobilitydb/jdbc/unit/boxes/TBoxTest.java
@@ -1,7 +1,6 @@
 package edu.ulb.mobilitydb.jdbc.unit.boxes;
 
 import edu.ulb.mobilitydb.jdbc.boxes.TBox;
-import edu.ulb.mobilitydb.jdbc.time.Period;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
