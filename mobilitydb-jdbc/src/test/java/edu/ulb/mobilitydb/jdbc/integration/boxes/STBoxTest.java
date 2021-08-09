d97652e2d757ebb2b798936b40ccc4534db1a6b7
Tatiana Millan <tatimilpo@gmail.com>
2021-08-09 17:25:11 +0200

    Add integration tests

--- mobilitydb-jdbc/src/test/java/edu/ulb/mobilitydb/jdbc/integration/boxes/STBoxTest.java
+++ mobilitydb-jdbc/src/test/java/edu/ulb/mobilitydb/jdbc/integration/boxes/STBoxTest.java
@@ -16,11 +16,20 @@ class STBoxTest extends BaseIntegrationTest {
 
     @ParameterizedTest
     @ValueSource(strings = {
-        ""
+        "STBOX ((1.0, 2.0), (1.0, 2.0))",
+        "STBOX Z((1.0, 2.0, 3.0), (1.0, 2.0, 3.0))",
+        "STBOX T((1.0, 2.0, 2001-01-03 00:00:00+01), (1.0, 2.0, 2001-01-03 00:00:00+01))",
+        "STBOX ZT((1.0, 2.0, 3.0, 2001-01-04 00:00:00+01), (1.0, 2.0, 3.0, 2001-01-04 00:00:00+01))",
+        "STBOX T(, 2001-01-03 00:00:00+01), (, 2001-01-03 00:00:00+01))",
+        "GEODSTBOX((11.0, 12.0, 13.0), (11.0, 12.0, 13.0))",
+        "GEODSTBOX T((1.0, 2.0, 3.0, 2001-01-03 00:00:00+01), (1.0, 2.0, 3.0, 2001-01-04 00:00:00+01))",
+        "GEODSTBOX T((, 2001-01-03 00:00:00+01), (, 2001-01-03 00:00:00+01))",
+        "SRID=5676;STBOX T((1.0, 2.0, 2001-01-04 00:00:00+01), (1.0, 2.0, 2001-01-04 00:00:00+01))",
+        "SRID=4326;GEODSTBOX((1.0, 2.0, 3.0), (1.0, 2.0, 3.0))"
     })
     void testConstructor(String value) throws SQLException {
         STBox stBox = new STBox(value);
-
+        
         PreparedStatement insertStatement = con.prepareStatement("INSERT INTO tbl_stbox(boxtype) VALUES (?);");
         insertStatement.setObject(1, stBox);
         insertStatement.execute();
